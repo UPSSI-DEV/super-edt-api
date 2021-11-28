2e22127a67cbd2bd7b628ef5bf4474484f05b2cf
Julian TRANI <73946832+JulianT31@users.noreply.github.com>
2021-11-28 09:31:59 +0100

    add switch end of the week

--- edt/edt.js
+++ edt/edt.js
@@ -1,123 +1,119 @@
-module.exports = { getEvents }
+module.exports = { getEvents };
 
-const google = require('googleapis')
-const calendar = new google.calendar_v3.Calendar()
-
-
+const google = require("googleapis");
+const calendar = new google.calendar_v3.Calendar();
 
 /* == Main function ==================================================================================== */
 
 async function getEvents(api_key, calendarList, bounds) {
-    let events = []
-    
-    for(const id of calendarList)
-        events = events.concat(await getRawData(api_key, id, bounds))
+    let events = [];
 
-    events = orderData(events)
-    events = splitData(events)
+    for (const id of calendarList)
+        events = events.concat(await getRawData(api_key, id, bounds));
 
-    return events
+    events = orderData(events);
+    events = splitData(events);
+
+    return events;
 }
-
-
 
 /* == Retrieve calendar events from google calendar ==================================================== */
 
 async function getRawData(api_key, calendarId, bounds) {
+    const calendarData = await calendar.events
+        .list({
+            key: api_key,
+            calendarId: calendarId,
+            timeMin: bounds[0],
+            timeMax: bounds[1],
+        })
+        .then((data) => data.data.items.filter((i) => i.status == "confirmed"));
 
-    const calendarData = await calendar.events.list({
-        key: api_key,
-        calendarId: calendarId,
-        timeMin: bounds[0],
-        timeMax: bounds[1]
-    }).then(data => data.data.items.filter(i => i.status == "confirmed"))
-
-    return calendarData
-
+    return calendarData;
 }
-
-
 
 /* == Retain only important information from calendar events =========================================== */
 
 function orderData(events) {
-    let orderedEvents = events.map(e => new Object({
-        raw: e.summary,
-        infos: parseEventName(e.summary),
-        start: e.start.date || e.start.dateTime,
-        end: e.end.date || e.end.dateTime,
-        group: e.organizer.displayName
-    })).sort((a,b) => {
-        const a_timestamp = (new Date(a.start)).getTime()
-        const b_timestamp = (new Date(b.start)).getTime()
-        return a_timestamp - b_timestamp
-    })
+    let orderedEvents = events
+        .map(
+            (e) =>
+                new Object({
+                    raw: e.summary,
+                    infos: parseEventName(e.summary),
+                    start: e.start.date || e.start.dateTime,
+                    end: e.end.date || e.end.dateTime,
+                    group: e.organizer.displayName,
+                })
+        )
+        .sort((a, b) => {
+            const a_timestamp = new Date(a.start).getTime();
+            const b_timestamp = new Date(b.start).getTime();
+            return a_timestamp - b_timestamp;
+        });
 
-    return orderedEvents
+    return orderedEvents;
 }
-
-
 
 /* == Splitting the data into different sections ======================================================= */
 
 function splitData(events) {
     const weeks = partition(events, (d) => {
-        const weekNum = getWeekNum(d.start)
-        return `S${weekNum}`
-    })
+        const weekNum = getWeekNum(d.start);
+        return `S${weekNum}`;
+    });
 
-    const days = {}
-    for(const w of Object.entries(weeks)) {
-        days[w[0]] = partition(w[1], (e) => new Date(e.start).toDateString())
+    const days = {};
+    for (const w of Object.entries(weeks)) {
+        days[w[0]] = partition(w[1], (e) => new Date(e.start).toDateString());
     }
 
-    return days
+    return days;
 }
 
-const partition = (events, getTag) => events.reduce((total, event) => {
-    const tag = getTag(event)
+const partition = (events, getTag) =>
+    events.reduce((total, event) => {
+        const tag = getTag(event);
 
-    if(!total[tag]) total[tag] = []
-    total[tag].push(event)
+        if (!total[tag]) total[tag] = [];
+        total[tag].push(event);
 
-    return total
-}, {})
+        return total;
+    }, {});
 
 function getWeekNum(timestamp) {
-    const curDate = new Date(timestamp)
+    const curDate = new Date(timestamp);
     var onejan = new Date(curDate.getFullYear(), 0, 1);
-    return Math.ceil((((curDate - onejan) / 86400000) + onejan.getDay() + 1) / 7);
+    return Math.ceil(((curDate - onejan) / 86400000 + onejan.getDay() + 1) / 7);
 }
 
-
-
-
-
 function parseEventName(name) {
-    const regexp = /(\*\*EXAMEN\*\*)? ?([A-Z]{5}\d[A-Z]\d)?(?: \( ?(.+)\))?(?: - )?(.*?)(?: - ([A-Z]\. .*|Xfreee?) - )(.*)/gm
-    const data = regexp.exec(name)
+    const regexp =
+        /(\*\*EXAMEN\*\*)? ?([A-Z]{5}\d[A-Z]\d)?(?: \( ?(.+)\))?(?: - )?(.*?)(?: - ([A-Z]\. .*|Xfreee?) - )(.*)/gm;
+    //const regexp =        /(\*\*EXAMEN\*\*)? ?([A-Z]{5}\d[A-Z]\d)?(?: \( ?(.+)\))?(?: - )?(.*?)(?: - ([A-Z]\.(\-[A-Z])?.*|Xfreee?) - )(.*)/gm;
+    const data = regexp.exec(name);
 
-    let info = {}
+    let info = {};
 
-    if(data == null) {
+    if (data == null) {
         info = {
             isExam: false,
             code: undefined,
-            type: 'Autre',
+            type: "Autre",
             lesson: name,
-            prof: 'UPSSI-prof',
-            room: 'Hors salle'
-        }
+            prof: "UPSSI-prof",
+            room: "Hors salle",
+        };
     } else {
         info = {
-            isExam: (data[1] != null),
+            isExam: data[1] != null,
             code: data[2],
-            type: (data[3] == undefined) ? 'Autre' : data[3],
+            type: data[3] == undefined ? "Autre" : data[3],
             lesson: data[4],
             prof: data[5],
-            room: data[6]
-        }
+            room: data[6],
+        };
     }
-    
-    return info
+
+    return info;
 }
--- index.js
+++ index.js
@@ -1,62 +1,84 @@
-const express = require('express')
-const cors = require('cors')
-const morgan = require('morgan')
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 
-const fs = require('fs')
-const https = require('https')
+const fs = require("fs");
+const https = require("https");
 
-const app = express()
+const app = express();
 
-app.use(morgan('tiny'))
-app.use(cors())
-app.use(express.json())
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
 
 // Read environment variables from .env file
-require('dotenv').config()
-require('./edt/date-util').setupDates()
-
-
+require("dotenv").config();
+require("./edt/date-util").setupDates();
 
 /* == ROUTING ===================================================================================== */
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Behold The MEVN Stack!' })
-})
+app.get("/", (req, res) => {
+    res.json({ message: "Behold The MEVN Stack!" });
+});
 
-const edt = require('./edt/edt')
-const links = require('./edt/links')
-const API_KEY = process.env.API_KEY
+const edt = require("./edt/edt");
+const links = require("./edt/links");
+const API_KEY = process.env.API_KEY;
 
-app.get('/events/week', async (req, res) => {
-  
-  // Get beginning and end of week
-  const week = (req.query['w']) ? req.query['w'] : new Date().getWeek()
-  const year = Date.schoolYear(week)
-  const bounds = [Date.dayOfWeek(year, week, 1), Date.dayOfWeek(year, week, 6)]
+app.get("/events/week", async (req, res) => {
+    // Get beginning and end of week
+    var week = req.query["w"] ? req.query["w"] : new Date().getWeek();
+    const currentDay = new Date().getDay();
 
-  // Get calendar list
-  const calendarList = [links.EDT_1A.COMMON, links.EDT_1A.MAIN, links.EDT_1A.GROUP_1]
+    // Check the current week is ending (saturday or sunday)
+    if (currentDay == 6) {
+        // Check if we are at the end of the year
+        if (week == 51) {
+            week = 1;
+        } else {
+            week++;
+        }
+    }
 
-  const events = await edt.getEvents(API_KEY, calendarList, bounds)
-  
-  res.json(events)
-})
+    const year = Date.schoolYear(week);
+    const bounds = [
+        Date.dayOfWeek(year, week, 1),
+        Date.dayOfWeek(year, week, 6),
+    ];
 
-app.get('/events/module/:code', (req, res) => {
-  // TODO: waiting for proper module parsing
-})
+    // Get calendar list
+    const calendarList = [
+        links.EDT_1A.COMMON,
+        links.EDT_1A.MAIN,
+        links.EDT_1A.GROUP_1,
+    ];
 
+    const events = await edt.getEvents(API_KEY, calendarList, bounds);
+    console.log(events);
 
+    res.json(events);
+});
+
+app.get("/events/module/:code", (req, res) => {
+    // TODO: waiting for proper module parsing
+});
 
 /* == SERVER LAUNCH =============================================================================== */
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
-if(process.env.PROD) {
-  const certificate = fs.readFileSync(process.env.SSL_CRT, 'utf-8')
-  const privateKey = fs.readFileSync(process.env.SSL_KEY, 'utf-8')
-  const credentials = {key: privateKey, cert: certificate}
+if (process.env.PROD) {
+    const certificate = fs.readFileSync(process.env.SSL_CRT, "utf-8");
+    const privateKey = fs.readFileSync(process.env.SSL_KEY, "utf-8");
+    const credentials = { key: privateKey, cert: certificate };
 
-  https.createServer(credentials, app).listen(port, console.log(`[🚀] HTTPS Server ready at https://localhost:${port}/`))
+    https
+        .createServer(credentials, app)
+        .listen(
+            port,
+            console.log(`[🚀] HTTPS Server ready at https://localhost:${port}/`)
+        );
 } else
-  app.listen(port, console.log(`[🚀] HTTP Server ready at http://localhost:${port}/`))
-
+    app.listen(
+        port,
+        console.log(`[🚀] HTTP Server ready at http://localhost:${port}/`)
+    );
