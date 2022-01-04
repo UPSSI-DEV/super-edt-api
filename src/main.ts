f1250a8bfa05f2c5459f0474d8bf7f910d5aa449
Guillaume Roussin <guillaume.roussin44@gmail.com>
2022-01-04 07:36:20 +0100

    Added calendar fetching

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,8 @@
-import DB from "./util/database";
-import genCalendars from "./init/calendars";
+import * as express from "express";
+const app = express();
 
-const calendarID = "jjr0au21evqc6guauvan3034ug@group.calendar.google.com";
+app.get("/auth/discord", (req, res) => {
+    res.json(req.query);
+});
 
-async function main() {
-    const db = await DB.get();
-    const modules = db.collection("modules");
-
-    modules.insertOne({ name: "SHS" });
-
-    console.log("Database opened");
-}
-
-//main();
-genCalendars();
+app.listen(4000, () => console.log("Backend launched"));
--- src/util/calendar-api.ts
+++ src/util/calendar-api.ts
@@ -1,7 +1,14 @@
 import { google, calendar_v3 } from "googleapis";
 import * as dotenv from "dotenv";
+import { GaxiosPromise } from "googleapis/build/src/apis/analyticsreporting";
 
-export default class CalendarAPI {
+interface CalendarEvent {
+    summary: string;
+    startTime: Date;
+    endTime: Date;
+}
+
+class CalendarAPI {
     private api: calendar_v3.Calendar;
     private key: string;
 
@@ -11,16 +18,56 @@ export default class CalendarAPI {
         this.api = google.calendar("v3");
     }
 
-    public async get(calendar_id: string, time_min: any, time_max: any) {
-        const eventList = await this.api.events
-            .list({
-                key: this.key,
-                calendarId: calendar_id
-                /*timeMin: time_min,
-                timeMax: time_max*/
-            })
-            .then((events) => events.data.items?.filter((x) => x.status === "confirmed"));
+    // Get the current school year bounds (year is considered to start and end in August)
+    private getYear() {
+        const THRESHOLD = 7; // August
 
-        return eventList;
+        const year = new Date().getFullYear();
+        const month = new Date().getMonth();
+        const schoolYears = month > THRESHOLD ? [year, year + 1] : [year - 1, year];
+
+        return schoolYears.map((x) => new Date(x, THRESHOLD, 1).toISOString());
+    }
+
+    // Requests all events from a school year for a specific calendar
+    private async fetchEvents(id: string, start: string, end: string) {
+        let events: calendar_v3.Schema$Event[] = [];
+        let nextPageToken = "";
+
+        do {
+            const result = await this.api.events
+                .list({
+                    key: this.key,
+                    calendarId: id,
+                    timeMin: start,
+                    timeMax: end,
+                    pageToken: nextPageToken
+                })
+                .then((x) => x.data);
+
+            nextPageToken = result.nextPageToken ?? "";
+            events = events.concat(result.items ?? []);
+        } while (events.length % 250 == 0);
+
+        return events.filter((x) => x.status === "confirmed");
+    }
+
+    public async get(calendar_id: string): Promise<Array<CalendarEvent>> {
+        const [startYear, endYear] = this.getYear();
+        const rawEvents = await this.fetchEvents(calendar_id, startYear, endYear);
+
+        const events: CalendarEvent[] = rawEvents.map((x) => ({
+            summary: x.summary ?? "",
+            startTime: new Date(`${x.start?.date || x.start?.dateTime}`),
+            endTime: new Date(`${x.end?.date || x.end?.dateTime}`)
+        }));
+
+        const year = (x: string) => new Date(x).getFullYear();
+        console.log(`current year: ${year(startYear)}/${year(endYear)}`);
+
+        return events.sort((a, b) => a.startTime.getTime() - b.startTime.getTime());
     }
 }
+
+export { CalendarAPI, CalendarEvent };
+export default CalendarAPI;
